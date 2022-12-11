716f0052b5b4d45e6e8ca4cd5fc9eae2b53b416d
catalyst1703 <vladsoroka2005@gmail.com>
2022-12-11 16:38:00 +0200

    add HotelAPI

---
+++ HostelAPI/HostelAPI/Data/HostelAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HostelAPI.Model;
+
+namespace HostelAPI.Data
+{
+    public class HostelAPIContext : DbContext
+    {
+        public HostelAPIContext (DbContextOptions<HostelAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HostelAPI.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ HostelAPI/HostelAPI/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostelAPI.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public UserRole Role { get; set; }
+    }
+}
